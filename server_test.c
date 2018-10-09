ac5b8a8233a33cfbdf7de310dee673f53a219c30
정소영 <starsuzi@naver.com>
2018-10-10 04:04:35 +0900

    server_test

--- server_test.c
+++ server_test.c
@@ -15,6 +15,32 @@ main(){
     int len;
     int s, new_s;
 
+    //build adderss data structure
+    bzero((char*)&sin, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(SERVER_PORT);
 
-    
+    //set up passive open
+    if((s = socket(PF_INET, SOCK_STREAM,0))<0){
+        perror("simplex-talk:socket");
+        exit(1);
+    } 
+    if((bind(s, (struct sockaddr *)&sin, sizeof(sin)))<0){
+        perror("simplex-talk: bind");
+        exit(1);
+    }
+    listen(s, MAX_PENDING);
+
+    //wait for connection, then receive and print text
+    while(1){
+        if((new_s = accept(s, (struct sockaddr *)&sin, &len))<0){
+            perror("simplex-talk: accept");
+            exit(1);
+        }
+        while(len = recv(new_s, buf, sizeof(buf), 0)){
+            fputs(buf, stdout);
+        }
+        close(new_s);
+    }
 }
