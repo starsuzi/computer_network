a23fb40bee97947184c42a0e4d0bf7482c678c6d
정소영 <starsuzi@naver.com>
2018-12-08 16:34:33 +0900

    updated 2016320120.c

--- 2016320120.c
+++ 2016320120.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #define SERVER_PORT 47500
 #define MAX_LINE 256
+#define MAX_FILE_SIZE 12288
 
 #define FLAG_HELLO		    ((uint8_t)(0x01 << 7))
 #define FLAG_INSTRUCTION	((uint8_t)(0x01 << 6))
@@ -16,6 +17,8 @@
 #define OP_ECHO		       	((uint8_t)(0x00))
 #define OP_INCREMENT		((uint8_t)(0x01))
 #define OP_DECREMENT		((uint8_t)(0x02))
+#define OP_PUSH			    ((uint8_t)(0x03))
+#define OP_DIGEST		    ((uint8_t)(0x04))
 
 struct hw_packet {
   uint8_t  flag;
@@ -27,12 +30,13 @@ struct hw_packet {
 
 void send_packet(int s, uint8_t flag, uint8_t op, uint16_t len, uint32_t seq, uint8_t *data);
 
-int main(int argc, char * argv[]) {
+int main(void) {
     FILE *fp;
     struct hostent *hp;
     struct sockaddr_in sin;
     char *host = "localhost";
-    int s;
+    int s, file_index = 0;
+    char file[MAX_FILE_SIZE];
 
     /* translate host name into peer's IP address */
     hp = gethostbyname(host);
@@ -74,7 +78,7 @@ int main(int argc, char * argv[]) {
     //recv
     recv(s, (char*) &rcvd_packet, sizeof(struct hw_packet), 0);
     printf("received hello message from the server!\n");
-	printf("waiting for the first instruction message...\n");
+	//printf("waiting for the first instruction message...\n");
     
     while(1) {
         
@@ -98,27 +102,23 @@ int main(int argc, char * argv[]) {
                 memcpy(&tmp, rcvd_packet.data, sizeof(uint32_t));
                 printf("operation type is echo.\n");
                 printf("echo : %s\n", rcvd_packet.data);
-                
             }
 
-            else if(rcvd_packet.operation != OP_ECHO) {
+            if(rcvd_packet.operation == OP_INCREMENT || rcvd_packet.operation == OP_DECREMENT) {
                 uint32_t tmp;
                 memcpy(&tmp, rcvd_packet.data, sizeof(uint32_t));
-				if(rcvd_packet.operation == OP_INCREMENT){
-					printf("operation type is increment\n");
-					tmp++;
-                    printf("increment : %d\n",tmp);
-				}
-				else{
-					printf("operation type is decrement\n");
-					tmp--;
-                     printf("decrement : %d\n",tmp);
-				}
-                //tmp += (rcvd_packet.operation == OP_INCREMENT) ? 1 : -1;
+                tmp += (rcvd_packet.operation == OP_INCREMENT) ? 1 : -1;
                 memcpy(rcvd_packet.data, &tmp, sizeof(uint32_t));
-            }
+                
+                send_packet(s, FLAG_RESPONSE, OP_ECHO, rcvd_packet.data_len, rcvd_packet.seq_num, rcvd_packet.data);
 
-            send_packet(s, FLAG_RESPONSE, OP_ECHO, rcvd_packet.data_len, rcvd_packet.seq_num, rcvd_packet.data);
+            } 
+            else if(rcvd_packet.operation == OP_PUSH){
+
+            }
+            else if(rcvd_packet.operation == OP_DIGEST){
+
+            }
         }
 
         if(rcvd_packet.flag == FLAG_TERMINATE){
@@ -136,6 +136,7 @@ void send_packet(int s, uint8_t flag, uint8_t op, uint16_t len, uint32_t seq, ui
     send_packet.operation = op;
     send_packet.data_len = len;
     send_packet.seq_num = seq;
+    if(data != NULL)
     memcpy(send_packet.data, data, len);
 
     //printf("send flag : %02X\n", send_packet.flag);
